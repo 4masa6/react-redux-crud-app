929d65547665c87f9b423c53c59c78fa45f10fbb
mogata <70381096+4masa6@users.noreply.github.com>
2022-05-08 10:36:50 +0900

    クラスコンポーネントとstate管理

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-function App() {
-  return (
-    <div>
-        <h1>Hello, world!</h1>
-    </div>
-  );
-}
+import {Component} from "react";
+
+
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 0}
+    }
+
+    onClickPlus = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    onClickMinus = () => {
+        this.setState({count: this.state.count - 1})
+    }
+
+    render() {
+        return (
+            <>
+                <div>{this.state.count}</div>
+                <button onClick={this.onClickPlus}>+1</button>
+                <button onClick={this.onClickMinus}>-1</button>
+            </>
+        )
+    }
+};
 
 export default App;
